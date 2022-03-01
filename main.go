705b25c8695fc64858b3633851259a301b5b899b
sbin0819 <sbinha123@gmail.com>
2022-03-01 10:14:36 +0900

    some changed

--- main.go
+++ main.go
@@ -2,16 +2,33 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"net/http"
 	"src/blockchain"
+	"text/template"
 )
 
-func main() {
-	chain := blockchain.GetBlockchain()
-	chain.AddBlock("Second")
-	chain.AddBlock("Third")
-	chain.AddBlock("Fourth")
+const port string = ":4000"
 
-	for _, block := range chain.AllBlocks() {
-		fmt.Printf("Data: %s\n", block.Data)
-	}
+type homeData struct {
+	PageTitle string
+	Blocks    []*blockchain.Block
+}
+
+func home(rw http.ResponseWriter, r *http.Request) {
+	tmpl := template.Must(template.ParseFiles("templates/home.html"))
+	data := homeData{"Home", blockchain.GetBlockchain().AllBlocks()}
+	tmpl.Execute(rw, data)
+}
+
+func user(rw http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(rw, "Hello Users!")
+}
+
+func main() {
+	http.HandleFunc("/", home)
+	http.HandleFunc("/users", user)
+
+	fmt.Printf("Listening on http://localhost:%s\n", port)
+	log.Fatal(http.ListenAndServe(port, nil))
 }
